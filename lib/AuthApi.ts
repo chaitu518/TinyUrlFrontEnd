53b2b6330f017efefa988567cc73bd3f06d5e942
chaitu518 <saichaitanya518@gmail.com>
2026-01-02 22:15:22 +0530

    made changes to envi variable

--- lib/AuthApi.ts
+++ lib/AuthApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const authApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 authApi.interceptors.request.use((config) => {
